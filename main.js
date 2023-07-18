fb706114c36658c5b7402278c892eeb29cb94134
Vinicius Andrade <viniciusy62@gmail.com>
2023-07-17 21:42:19 -0300

    update functions

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ const weight = document.querySelector("#weight");
 const height = document.querySelector("#height");
 const errorMsg = document.querySelector(".error");
 
+
+// Object Literal
+const Modal = {
+  alertClose: document.querySelector(".modal-wrapper"),
+  alert: document.querySelector(".modal-wrapper"),
+  result: document.querySelector("#result"),
+
+  open() {
+    Modal.alert.classList.add("open");
+  },
+  close() {
+    Modal.alertClose.classList.remove("open");
+  }
+}
+
 function validate(e) {
   const value = e.target.value
 
@@ -18,32 +33,27 @@ weight.addEventListener("input", validate)
 height.addEventListener("input", validate)
 
 function calcIMC() {
-  let calc = parseInt(weight.value / (height.value / 100) ** 2);
+  let calc = (weight.value / (height.value / 100) ** 2).toFixed(2);
   
   return calc;
 }
 
 
 function closeScreen() {
-  const modalAlertClose = document.querySelector(".modal-wrapper");
-  modalAlertClose.classList.remove("open");
+  Modal.close()
 }
 
 
 function alertIMC(e) {
   e.preventDefault();
 
-  const modalAlert = document.querySelector(".modal-wrapper");
-  const result = document.querySelector("#result");
-
   if(isNaN(calcIMC())) {
     errorMsg.classList.add("open");
   } else {
     errorMsg.classList.remove("open");
 
-    modalAlert.classList.add("open");
-    result.innerHTML = `<h1>Seu IMC é de ${calcIMC()}</h1>`;
-    console.log(calcIMC())
+    Modal.open()
+    Modal.result.innerHTML = `<h1>Seu IMC é de ${calcIMC()}</h1>`;
   }
   weight.value = ""
   height.value = ""
